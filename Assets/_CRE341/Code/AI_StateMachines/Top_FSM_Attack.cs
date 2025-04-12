4d19c8496194a7c8c3396b545eda1d24cc798648
Ccampbell32 <campbell_McFadden-c@ulster.ac.uk>
2025-04-12 19:41:36 +0100

    attack updates

--- Assets/_CRE341/Code/AI_StateMachines/Top_FSM_Attack.cs
+++ Assets/_CRE341/Code/AI_StateMachines/Top_FSM_Attack.cs
@@ -10,7 +10,7 @@ public class Top_FSM_Attack : StateMachineBehaviour
         AI_FSM aiFSM = animator.GetComponent<AI_FSM>();
 
         // Start the attack animation 
-      
+
         animator.SetTrigger("Attack");
 
         // Optionally, you can set a flag to indicate that the attack is in progress
@@ -20,7 +20,7 @@ public class Top_FSM_Attack : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+
         AI_FSM aiFSM = animator.GetComponent<AI_FSM>();
 
         // Checking if the attack animation is finished
@@ -29,16 +29,33 @@ public class Top_FSM_Attack : StateMachineBehaviour
             // Reset the attack flag
             aiFSM.isAttacking = false;
 
-           
+
+        }
+
+        // Find the player object
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        // Check if the player is within range (you'll need to implement this part)
+        if (IsPlayerInRange(player, aiFSM.transform))
+        {
+            // Deal damage to the player
+            player.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
         }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+
         AI_FSM aiFSM = animator.GetComponent<AI_FSM>();
 
         // Reset the attack flag
         aiFSM.isAttacking = false;
+    }
+
+    // You'll need to implement this method to check if the player is within range
+    private bool IsPlayerInRange(GameObject player, Transform aiTransform)
+    {
+        // Implement your range check logic here (e.g., raycast, collider check)
+        return false; // Replace with your actual range check
     }
 }
